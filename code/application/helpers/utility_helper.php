f12f95ee4901003d34f42aadd22581985c3d16e4
kkazmi <kkazmi@github.com>
2018-01-19 12:57:22 +0530

    updated

---
+++ code/application/helpers/utility_helper.php
@@ -0,0 +1,42 @@
+
+<?php
+function asset_url()
+{
+   return base_url().'assets/';
+}
+function logo_url()
+{
+	return base_url().'assets/images/crypto-cash-coin.png';
+}
+
+function favicon_url()
+{
+	return base_url().'assets/images/crypto-cash-favicon.png';
+}
+function login_bg()
+{
+	return base_url().'assets/images/loginbg.jpg';
+}
+
+function project_name()
+{
+	return 'Crypto Cash';
+}
+
+function wallet_mail()
+{
+	return 'support@wallet.io';
+}
+
+function sending_mail()
+{
+	return 'prateekvermaphp@gmail.com';
+}
+
+function sending_mail_pass()
+{
+	return '@prateek123';
+}
+
+
+?>
